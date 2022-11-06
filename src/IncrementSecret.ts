30e535797a7c803c48d2a1bb2568d40e0c705cd9
fork <fork@forks-MacBook-Pro.local>
2022-11-06 00:29:46 -0700

    Anon Dao Voting, proposal passes with a threshold of 2 !

---
+++ src/IncrementSecret.ts
@@ -0,0 +1,121 @@
+import {
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  DeployArgs,
+  Poseidon,
+  Permissions,
+  isReady,
+  Encoding,
+  PrivateKey,
+  PublicKey
+} from 'snarkyjs';
+
+export { isReady, Field, Encoding };
+  
+// Wait till our SnarkyJS instance is ready
+await isReady;
+
+export const users = {
+  Bob: PrivateKey.fromBase58(
+    "EKFAdBGSSXrBbaCVqy4YjwWHoGEnsqYRQTqz227Eb5bzMx2bWu3F"
+  ),
+  SuperBob: PrivateKey.fromBase58(
+    "EKEitxmNYYMCyumtKr8xi1yPpY3Bq6RZTEQsozu2gGf44cNxowmg"
+  ),
+  MegaBob: PrivateKey.fromBase58(
+    "EKE9qUDcfqf6Gx9z6CNuuDYPe4XQQPzFBCfduck2X4PeFQJkhXtt"
+  ), // This one says duck in it :)
+  Jack: PrivateKey.fromBase58(
+    "EKFS9v8wxyrrEGfec4HXycCC2nH7xf79PtQorLXXsut9WUrav4Nw"
+  ),
+};
+
+
+export class IncrementSecret extends SmartContract {
+  @state(Field) message = State<Field>();
+  @state(Field) VoteCount = State<Field>();
+  @state(PublicKey) user1 = State<PublicKey>();
+  @state(PublicKey) user2 = State<PublicKey>();
+  @state(PublicKey) user3 = State<PublicKey>();
+
+  deploy(args: DeployArgs) {
+    super.deploy(args);
+    this.setPermissions({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+    });
+  }
+
+  @method init() {
+    this.user1.set(users['Bob'].toPublicKey());
+    this.user2.set(users['SuperBob'].toPublicKey());
+    this.user3.set(users['MegaBob'].toPublicKey());
+    this.message.set(Field(0));
+    this.VoteCount.set(Field(0));
+
+  }
+
+
+
+  @method publishMessage(message: Field, signerPrivateKey: PrivateKey) {
+    // Compute signerPublicKey from signerPrivateKey argument
+    const signerPublicKey = signerPrivateKey.toPublicKey();
+    // Get approved public keys
+    const user1 = this.user1.get();
+    this.user1.assertEquals(this.user1.get());
+    const user2 = this.user2.get();
+    this.user2.assertEquals(this.user2.get());
+    const user3 = this.user3.get();
+    this.user3.assertEquals(this.user3.get());
+    // Assert that signerPublicKey is one of the approved public keys
+    signerPublicKey
+    .equals(user1)
+    .or(signerPublicKey.equals(user2))
+    .or(signerPublicKey.equals(user3))
+    .assertEquals(true);
+    // Update on-chain message variable
+    this.message.set(message);
+
+}
+
+@method voteonMessage(voterPrivateKey: PrivateKey){
+  const signerPublicKey = voterPrivateKey.toPublicKey();
+    // Get approved public keys
+    const user1 = this.user1.get();
+    this.user1.assertEquals(this.user1.get());
+    const user2 = this.user2.get();
+    this.user2.assertEquals(this.user2.get());
+    const user3 = this.user3.get();
+    this.user3.assertEquals(this.user3.get());
+    // Assert that signerPublicKey is one of the approved public keys
+    signerPublicKey
+    .equals(user1)
+    .or(signerPublicKey.equals(user2))
+    .or(signerPublicKey.equals(user3))
+    .assertEquals(true);
+
+  const currentMessage = this.message.get();
+  this.message.assertEquals(this.message.get());
+
+  const currentVoterCount = this.VoteCount.get();
+  this.VoteCount.assertEquals(this.VoteCount.get());
+  const latestVoteCount = currentVoterCount.add(1);
+  this.VoteCount.set(currentVoterCount.add(1));
+
+  }
+
+  @method checkVotes(){
+      // Pass the DAO Vote at threashold 2
+      // Do stuff at this point, we set voteCount to 1337 as pass condition
+
+  this.VoteCount.assertEquals(Field.fromNumber(2));
+  this.VoteCount.set(Field.fromNumber(1337));
+  }
+
+
+}
+
+  
---
+++ src/main.ts
@@ -0,0 +1,163 @@
+import { IncrementSecret } from './IncrementSecret.js';
+import {
+  isReady,
+  shutdown,
+  Field,
+  Mina,
+  PrivateKey,
+  PublicKey,
+  AccountUpdate,
+  Poseidon,
+} from 'snarkyjs';
+
+export const users = {
+  Bob: PrivateKey.fromBase58(
+    "EKFAdBGSSXrBbaCVqy4YjwWHoGEnsqYRQTqz227Eb5bzMx2bWu3F"
+  ),
+  SuperBob: PrivateKey.fromBase58(
+    "EKEitxmNYYMCyumtKr8xi1yPpY3Bq6RZTEQsozu2gGf44cNxowmg"
+  ),
+  MegaBob: PrivateKey.fromBase58(
+    "EKE9qUDcfqf6Gx9z6CNuuDYPe4XQQPzFBCfduck2X4PeFQJkhXtt"
+  ), // This one says duck in it :)
+  Jack: PrivateKey.fromBase58(
+    "EKFS9v8wxyrrEGfec4HXycCC2nH7xf79PtQorLXXsut9WUrav4Nw"
+  ),
+};
+
+(async function main() {
+  await isReady;
+
+  
+
+  console.log('SnarkyJS loaded');
+
+  
+
+  const Local = Mina.LocalBlockchain();
+  Mina.setActiveInstance(Local);
+  const deployerAccount = Local.testAccounts[0].privateKey;
+
+  const salt = Field.random();
+
+  
+  // ----------------------------------------------------
+
+  // create a destination we will deploy the smart contract to
+  const zkAppPrivateKey = PrivateKey.random();
+  const zkAppAddress = zkAppPrivateKey.toPublicKey();
+
+  // create an instance of IncrementSecret - and deploy it to zkAppAddress
+  const zkAppInstance = new IncrementSecret(zkAppAddress);
+  const deploy_txn = await Mina.transaction(deployerAccount, () => {
+    AccountUpdate.fundNewAccount(deployerAccount);
+    zkAppInstance.deploy({ zkappKey: zkAppPrivateKey });
+    zkAppInstance.init();
+    zkAppInstance.sign(zkAppPrivateKey);
+  });
+  await deploy_txn.send().wait();
+
+  // get the initial state of IncrementSecret after deployment
+  // const num0 = zkAppInstance.x.get();
+  // console.log('state after init:', num0.toString());
+
+  // ----------------------------------------------------
+
+
+
+  console.log("Bob in the group , this will pass"); 
+  const txn3 = await Mina.transaction(deployerAccount, () => {
+    zkAppInstance.publishMessage(Field.fromNumber(750),users.Bob);
+    zkAppInstance.sign(zkAppPrivateKey);
+  });
+  await txn3.send().wait();
+
+  const num3 = zkAppInstance.message.get();
+  console.log('Message:', num3.toString());
+
+  // ----------------------------------------------------
+
+     console.log("SuperBob in the group , this will pass"); 
+     const txn6 = await Mina.transaction(deployerAccount, () => {
+       zkAppInstance.publishMessage(Field.fromNumber(850),users.SuperBob);
+       zkAppInstance.sign(zkAppPrivateKey);
+     });
+     await txn6.send().wait();
+
+
+
+  const num6 = zkAppInstance.message.get();
+  console.log('Message:', num6.toString());
+
+   // ----------------------------------------------------
+  console.log("MegaBob is in the group , this will not revert");
+  const txn5 = await Mina.transaction(deployerAccount, () => {
+   zkAppInstance.publishMessage(Field.fromNumber(950),users.MegaBob);
+   zkAppInstance.sign(zkAppPrivateKey);
+ });
+ try{
+   await txn5.send().wait();
+   const num5 = zkAppInstance.message.get();
+   console.log('Message:', num5.toString());
+ }catch(ex){
+   console.log(ex);
+ }
+
+
+   // ----------------------------------------------------
+
+  console.log("Jack is not in the group , will try update message to 1050; this will revert");
+  console.log("core dump , need better error handling") 
+  // const txn4 = await Mina.transaction(deployerAccount, () => {
+  //   zkAppInstance.publishMessage(Field.fromNumber(1050),users.Jack);
+  //   zkAppInstance.sign(zkAppPrivateKey);
+  // });
+  // try{
+  //   await txn4.send().wait();
+  //   const num4 = zkAppInstance.message.get();
+  //   console.log('state after txn4:', num4.toString());
+  // }catch(ex){
+  //   console.log(ex);
+  // }
+
+  // ----------------------------------------------------
+
+  console.log("SuperBob Voting on Message"); 
+  const txn9 = await Mina.transaction(deployerAccount, () => {
+    zkAppInstance.voteonMessage(users.SuperBob);
+    zkAppInstance.sign(zkAppPrivateKey);
+  });
+  await txn9.send().wait();
+
+  const num9 = zkAppInstance.VoteCount.get();
+  console.log('VoteCount:', num9.toString());
+
+  // ----------------------------------------------------
+
+  console.log("MegaBob Voting on Message"); 
+  const txn10 = await Mina.transaction(deployerAccount, () => {
+    zkAppInstance.voteonMessage(users.MegaBob);
+    zkAppInstance.sign(zkAppPrivateKey);
+  });
+  await txn10.send().wait();
+
+  const num10= zkAppInstance.VoteCount.get();
+  console.log('VoteCount:', num10.toString());
+
+
+    // ----------------------------------------------------
+
+    console.log("Check if Vote passed with a threshold of 2"); 
+    const txn11 = await Mina.transaction(deployerAccount, () => {
+      zkAppInstance.checkVotes();
+      zkAppInstance.sign(zkAppPrivateKey);
+    });
+    await txn11.send().wait();
+  
+    const num11= zkAppInstance.VoteCount.get();
+    console.log('Vote Passed, Yaay!:', num11.toString());
+
+  console.log('Shutting down');
+
+  await shutdown();
+})();
